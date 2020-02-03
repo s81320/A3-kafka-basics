51af35c2235163b4469c4f3948a122a9213afc02
Silke <58777304+s81320@users.noreply.github.com>
2020-02-03 11:24:40 +0100

    Update A3-producerA.py
    removed whitespaces

--- A3-producerA.py
+++ A3-producerA.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python
 
 from pykafka import KafkaClient # start with pip install pykafka
-
 import time
 
-
 # KAFKA CLIENT , TOPIC , PRODUCER
-client = KafkaClient(hosts='127.0.0.1:9092')
-topic = client.topics['A3-homework']
-producer = topic.get_sync_producer()
+client=KafkaClient(hosts='127.0.0.1:9092')
+topic=client.topics['A3-homework']
+producer=topic.get_sync_producer()
 
 # send integers
-i = 2
+i=2
 while True:
 	producer.produce(str(i).encode('utf-8'))
-	i += 2
+	i+=2
 	time.sleep(1)
 
