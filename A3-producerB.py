3eb5ac1c41e4b1f4bd4292d8b43978b6b055d22c
Silke <58777304+s81320@users.noreply.github.com>
2020-02-03 11:26:08 +0100

    Update A3-producerB.py
    removed comments (work in progress) and white spaces :-)

--- A3-producerB.py
+++ A3-producerB.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python
 
 from pykafka import KafkaClient # start with pip install pykafka
-
 import time
 
-
 # KAFKA CLIENT , TOPIC , PRODUCER
-client = KafkaClient(hosts='127.0.0.1:9092')
-topic = client.topics['A3-homework']
-producer = topic.get_sync_producer()
-
-# SEND TEXT
-#producer.produce('my text, from a python script'.encode('ascii'))
-#time.sleep(1)
-#producer.produce('something new, something blue'.encode('utf-8'))
-# how to send an integer??
-#producer.produce('345'.encode('utf-8'))
+client=KafkaClient(hosts='127.0.0.1:9092')
+topic=client.topics['A3-homework']
+producer=topic.get_sync_producer()
 
 # send integers
-i = 1
+i=1
 while True:
 	producer.produce(str(i).encode('utf-8'))
-	i+= 2
+	i+=2
 	time.sleep(2)
