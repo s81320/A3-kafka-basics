181c11315ec10398dac20f7c38b5b067f60eac24
Silke <58777304+s81320@users.noreply.github.com>
2020-02-03 11:23:50 +0100

    Update A3-consumer.py
    removed comments

--- A3-consumer.py
+++ A3-consumer.py
@@ -10,9 +10,3 @@ consumer = topic.get_simple_consumer()
 # DO SOMETHING
 for c in consumer:
 	print(c.offset , ' : ' , c.value)
-
-#'compression_type', 'decode', 'delivery_report_q', 'offset', 'pack_into', 'partition', 
-# 'partition_id', 'partition_key', 'produce_attempt', 'protocol_version', 'set_timestamp', 
-# 'timestamp', 'timestamp_dt', 'value'	
-
-# c is a pykafka.protocol.message.Message object
